f538e10d27ed56c1f4b0471a5b52ca77ae40e48d
Michelle Rogers <micheknows@gmail.com>
2022-09-13 22:24:10 -0600

    Added functionality for present and absent student lists with buttons to change from absent/present and vice versa

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,23 @@
 public class Student extends Person{
 
     boolean isPresent;
+    boolean called;
 
     public Student(){
         setPresent(true);
+        setCalled(false);
     }
 
     public Student(String wholename) {
         setPresent(true);
+        setCalled(false);
         String[] namelist = wholename.trim().split("\\s+");
         this.setFirstName(namelist[0]);
         this.setLastName(namelist[1]);
+    }
+
+    public String getWholeName(){
+        return this.firstName + " " + this.lastName;
     }
 
     public Student(boolean present){
@@ -20,8 +27,10 @@ public class Student extends Person{
     public boolean getPresent(){
         return isPresent;
     }
+    public boolean getCalled() { return this.called;}
 
     public void setPresent(boolean present) {
         isPresent = present;
     }
+    public void setCalled(boolean called) {this.called = called;}
 }
